7cae1612f06a28c3ebfc77885a2e66ef601b7f9c
codysharma <codysharma01@gmail.com>
2020-05-31 22:39:31 +0530

    Added emulation play/pause button

--- includes/main.h
+++ includes/main.h
@@ -2,40 +2,25 @@
 #define MAIN_H
 
 #include <SDL2/SDL.h>
-#define SCALE 10
+#define SCALE 16
 #define STACKLEVEL 16
-#define RAMSIZE  4096
+#define RAMSIZE 4096
 #define WIDTH (SCALE * 64)
 #define HEIGHT (SCALE * 32)
-#define PIXELS  (WIDTH*HEIGHT)
+#define PIXELS (WIDTH * HEIGHT)
 #define ROMSTART 0x200
 #define HIGHMEM 0xfff
 #define FONTSTART 0x50
 #define FONTSETSIZE 80
 
+typedef enum
+{
+    false,
+    true
+} bool;
 
-typedef enum {
-    false, true
-}bool;
-
-typedef unsigned char byte; //for 8bits(1 Byte) values
-typedef unsigned short int word;    //for 16bits(2 Bytes) values
-
-// #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__      //check for endianness with help of gcc compiler options
-//     #define WORDS_BIGENDIAN
-// #endif
-// typedef union
-// {
-//     unsigned short int WORD;
-//     struct
-//     {
-//         #ifdef WORDS_BIGENDIAN                  //setting order of bits depending on endianness
-//             byte high, low;
-//         #else
-//             byte low, high;
-//         #endif
-//     } BYTE;
-// } word;
+typedef unsigned char byte;      //for 8bits(1 Byte) values
+typedef unsigned short int word; //for 16bits(2 Bytes) values
 
 typedef struct
 {
@@ -44,20 +29,17 @@ typedef struct
     word pc;      // Program Couner
     word sp;      // Stack Pointer
     byte dt;      // Delay Timer
-    byte st;      // Sound Timer 
-    word opCode; // Opcode
+    byte st;      // Sound Timer
+    word opCode;  // Opcode
 } chip8regset;
 
-// // #include <stdint.h>
-// extern byte fontset[80];
-
-// //cpu stuffs
-extern byte memory[RAMSIZE];  //4KB of RAM
+// cpu stuffs
+extern byte memory[RAMSIZE];   //4KB of RAM
 extern word stack[STACKLEVEL]; //16levels for Subroutine (function calls)
-extern byte gfx[PIXELS];        // Total amount of pixels: 2048
-extern byte key[16];             // keys to be pressed from 0-F
+extern byte gfx[PIXELS];       // Total amount of pixels: 2048
+extern byte key[16];           // keys to be pressed from 0-F
 extern bool drawFlag;
-
+extern bool paused;
 //graphics stuffs
 extern SDL_Renderer *renderer;
 extern SDL_Window *window;
@@ -70,5 +52,5 @@ void chip8Init(chip8regset *cpu);
 void chip8LoadGame(const char *fileName);
 void chip8EmulateCycle(chip8regset *cpu);
 void graphicsLoop(const char *filename, chip8regset *cpu);
-void draw(void);
+void graphicsDraw(void);
 #endif
